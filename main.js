55a70514e63f069355922ee1b9772c8322f9d8d7
ahmedmmeky <meky.wrk@gmail.com>
2021-09-16 15:41:58 -0400

    updated projects section; updated image src's to hosted images; dynamically showing projects section; scrolling to each section of the page from the header; updated resume; removed unused images.

--- main.js
+++ main.js
@@ -41,6 +41,99 @@ const projects = [
       "Website for kids and parents to read interactive story books, allows children to express how they feel with feedback at the end of each story, provides a blog, coloring books, and videos.",
     link: "https://www.penandpals.com/",
   },
+  {
+    imgSrc: "https://i.ibb.co/bHx5zCZ/Screen-Shot-2021-09-15-at-4-17-27-PM.png",
+    title: "Sample Client Webpage",
+    stack: "Vanilla JS | HTML/CSS",
+    intro:
+      "Built a website template to use for informational web pages; has scroll feature to scroll to each section of the page.",
+    link: "/10-scroll/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/ZXYf33S/Screen-Shot-2021-09-15-at-4-21-37-PM.png",
+    title: "Restaurant Menu",
+    stack: "Vanilla JS | HTML/CSS",
+    intro:
+      "Sample restaurant website for restaurant to dynamically add menu items, and categorize them.",
+    link: "/08-menu/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/GtvHszM/Screen-Shot-2021-09-15-at-1-58-37-PM.png",
+    title: "Grocery List",
+    stack: "Vanilla JS | HTML/CSS",
+    intro:
+      "Basic grocery list project to add, edit, and delete grocery items to a list.",
+    link: "/14-grocery-bud/index.html",
+  },
+
+  {
+    imgSrc: "https://i.ibb.co/2c3KfJ6/Screen-Shot-2021-09-15-at-4-40-50-PM.png",
+    title: "Color Flipper",
+    stack: "Vanilla JS | HTML/CSS",
+    intro: "Randomly generates colors with regular identifiers and hex code.",
+    link: "/01-color-flipper/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/yYzSRXk/Screen-Shot-2021-09-15-at-4-38-00-PM.png",
+    title: "Customer Reviews",
+    stack: "Vanilla JS | HTML/CSS",
+    intro:
+      "Sample testimonials component for a customer reviews section. Dynamically shows each review.",
+    link: "/03-reviews/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/w0GLD3S/Screen-Shot-2021-09-15-at-4-20-10-PM.png",
+    title: "Video Coverpage",
+    stack: "Vanilla JS | HTML/CSS",
+    intro:
+      "Sample video with backdrop which can be used as part of a homepage.",
+    link: "/09-video/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/QcZF2yk/Screen-Shot-2021-09-15-at-4-23-42-PM.png",
+    title: "Questions Component",
+    stack: "Vanilla JS | HTML/CSS",
+    intro:
+      "Questions component for companies, products, or pretty much any app that needs a FAQ section!",
+    link: "/07-questions/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/4d8RGdy/Screen-Shot-2021-09-15-at-4-15-33-PM.png",
+    title: "Tabs Component",
+    stack: "Vanilla JS | HTML/CSS",
+    intro:
+      "Used to group common information into categories; useful for a nice and sleek way to group information.",
+    link: "/11-tabs/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/GH4m8pR/Screen-Shot-2021-09-15-at-4-11-02-PM.png",
+    title: "Paragraph Generator",
+    stack: "Vanilla JS | HTML/CSS",
+    intro: "Used as a replacement to the lorem septum placeholder text.",
+    link: "/13-lorem-ipsum/index.html",
+  },
+
+  {
+    imgSrc: "https://i.ibb.co/HV3rq9g/Screen-Shot-2021-09-15-at-4-26-06-PM.png",
+    title: "Modal/Dialog Component",
+    stack: "Vanilla JS | HTML/CSS",
+    intro: "",
+    link: "/06-modal/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/xC11sKn/Screen-Shot-2021-09-15-at-4-39-05-PM.png",
+    title: "Counter",
+    stack: "Vanilla JS | HTML/CSS",
+    intro: "",
+    link: "/02-counter/index.html",
+  },
+  {
+    imgSrc: "https://i.ibb.co/nPNkzf5/Screen-Shot-2021-09-15-at-4-13-38-PM.png",
+    title: "Countdown Timer",
+    stack: "Vanilla JS | HTML/CSS",
+    intro: "Sample countdown website with days, hours, minutes, seconds.",
+    link: "/12-coutdown-timer/index.html",
+  },
 ];
 
 let theme = localStorage.getItem("theme");
@@ -48,6 +141,15 @@ let theme = localStorage.getItem("theme");
 let themeDots = document.getElementsByClassName("theme-dot");
 
 const postWrapper = document.querySelector(".post-wrapper");
+
+const projectsLink = document.getElementById("projects");
+
+const projectHeading = document.querySelector(".project-heading");
+
+const contactLink = document.getElementById("contact");
+const contactHeading = document.querySelector(".contact-heading");
+
+const greetingWrapper = document.querySelector(".greeting-wrapper");
 
 if (theme == null) {
   setTheme("light");
@@ -85,6 +187,30 @@ function setTheme(mode) {
 
 window.addEventListener("DOMContentLoaded", function () {
   displayProjectPosts(projects);
+});
+
+projectsLink.addEventListener("click", function () {
+  const greetingWrapperHeight = greetingWrapper.getBoundingClientRect().height;
+  let position = projectHeading.offsetTop - greetingWrapperHeight;
+
+  position = position + greetingWrapperHeight;
+
+  window.scrollTo({
+    left: 0,
+    top: position,
+  });
+});
+
+contactLink.addEventListener("click", function () {
+  const greetingWrapperHeight = greetingWrapper.getBoundingClientRect().height;
+  let position = contactHeading.offsetTop - greetingWrapperHeight;
+
+  position = position + greetingWrapperHeight;
+
+  window.scrollTo({
+    left: 0,
+    top: position,
+  });
 });
 
 function displayProjectPosts(projects) {
